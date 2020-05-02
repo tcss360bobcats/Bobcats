3f8ba0fc362631cbb76e66fb8fd5103e4d9c8e73
Anthony <nguyenanthony80@gmail.com>
2020-05-02 13:43:19 -0700

    added about class

---
+++ HomeManual/src/utilities/About.java
@@ -0,0 +1,52 @@
+package utilities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Anthony Nguyen
+ * @version May 5, 2020 
+ *
+ */
+public class About {
+
+	private String[] authors = {"Darryl James", "Andrew Lim", "Tyke Sykes", "Anthony Nguyen"};
+	
+	
+	/**
+	 * Run this to update the current version of the project. 
+	 * @param args
+	 * @throws FileNotFoundException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		updateVersion();
+	}
+	
+	
+	public static void updateVersion() throws FileNotFoundException, UnsupportedEncodingException {
+		String updateDate = LocalDate.now().toString();
+		PrintWriter writer = new PrintWriter("files/version.txt", "UTF-8");
+		writer.println(updateDate);
+		System.out.println(updateDate);
+		writer.close();
+	}
+	
+	public String getVersion() throws IOException {
+		Scanner file = new Scanner(new File("files/version.txt"));
+		String version = file.nextLine();
+		file.close();
+		return version;
+	}
+	
+	public String[] getAuthors() {
+		return authors;
+	}
+	
+}
