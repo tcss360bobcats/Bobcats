5b2bb3820778c412b5e8553a70dce265f93442d9
Tyke Sykes <sykestylerd@gmail.com>
2020-05-08 14:57:03 -0700

    added File and Room classes

---
+++ HomeManual/src/model/Room.java
@@ -0,0 +1,108 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A Room object contains a List of Items and has a name. There can be duplicate Items in a Room.
+ * 
+ * @author Tyke Sykes
+ * @version 5/08/2020
+ */
+public class Room {
+	/** The name of the Room. */
+	private String myName;
+	
+	/** The List of Items in the Room. */
+	private ArrayList<Item> myItems;
+	
+	/**
+	 * Constructor for a Room with no Items.
+	 * 
+	 * @param theName is the name of the Room.
+	 */
+	public Room(String theName) {
+		this(theName, new ArrayList<Item>());
+	}
+	
+	/**
+	 * Constructor for a Room with Items.
+	 * 
+	 * @param theName is the name of the Room.
+	 * @param theItems is the List of Items in the Room.
+	 */
+	public Room(String theName, ArrayList<Item> theItems) {
+		myName = theName;
+		myItems = (ArrayList<Item>) theItems.clone();
+	}
+	
+	/**
+	 * Sets the name of the Room.
+	 * 
+	 * @param theName is the new name to be set.
+	 */
+	public void setName(String theName) {
+		myName = theName;
+	}
+	
+	/**
+	 * Returns the name of the Room.
+	 * 
+	 * @return the name of the Room.
+	 */
+	public String getName() {
+		return myName;
+	}
+	
+	/**
+	 * Adds an Item to the room.
+	 * 
+	 * @param theItem is the Item to be added to the Room.
+	 */
+	public void addItem(Item theItem) {
+		myItems.add(theItem);
+	}
+	
+	/**
+	 * Adds multiple Items to the room.
+	 * 
+	 * @param theItems is the list of Items to be added to the Room.
+	 */
+	public void addItems(List<Item> theItems) {
+		myItems.addAll(theItems);
+	}
+	
+	/**
+	 * Removes an Item from the Room.
+	 * 
+	 * @param theItem is the Item to be removed to the Room.
+	 */
+	public void removeItem(Item theItem) {
+		myItems.remove(theItem);
+	}
+	
+	/**
+	 * Removes multiple Items from the Room.
+	 * 
+	 * @param theItems is the List of Items to be removed from the Room.
+	 */
+	public void removeItems(ArrayList<Item> theItems) {
+		myItems.removeAll(theItems);
+	}
+	
+	/**
+	 * Removes all Items from the Room.
+	 */
+	public void clearItems() {
+		myItems.clear();
+	}
+	
+	/**
+	 * Returns the List of Items in the Room.
+	 * 
+	 * @return the List of Items in the Room.
+	 */
+	public List<Item> getItems() {
+		return myItems;
+	}
+}
