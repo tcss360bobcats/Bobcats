05dd4ef589cee82b12b6f0dd61a2e04157d60972
Anthony <nguyenanthony80@gmail.com>
2020-05-08 13:29:21 -0700

    ui change

---
+++ HomeManual/src/view/ManualMain.java
@@ -0,0 +1,27 @@
+package view;
+import java.awt.EventQueue;
+
+public final class ManualMain {
+	
+	/**
+	 * Private constructor to prevent instantiation
+	 */
+	private ManualMain() {
+		//do nothing
+	}
+	
+	/**
+	 * Constructs the main GUI window frame
+	 * 
+	 * @param theArgs Command line arguments (ignored).
+	 */
+	public static void main(String[] theArgs) {
+		
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ManualGUI();     
+            }
+        });
+	}
+}
