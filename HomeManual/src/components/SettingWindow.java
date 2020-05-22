978d7fa0641058ca47a1b23f2584a1713a5d6ee3
Anthony <nguyenanthony80@gmail.com>
2020-05-22 14:47:00 -0700

    added a setting window popup

---
+++ HomeManual/src/components/SettingWindow.java
@@ -0,0 +1,83 @@
+package components;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SettingWindow extends JFrame {
+
+	
+	public SettingWindow() {
+		setSize(300, 250);
+		add(createWindow());
+		setVisible(true);
+	}
+	
+	/**
+	 * Creates a settings window
+	 * @author Anthony
+	 * @return Jpanel with settings options
+	 */
+	public JPanel createWindow() {
+		JPanel master = new JPanel();
+		
+		
+		//TODO Add action listeners to the buttons to import/export/change settings
+		JButton importButton = new JButton("Import Settings");
+		JButton exportButton = new JButton("Export Settings");
+		JButton saveButton = new JButton("Update Settings");
+		
+		
+		
+		master.add(nameSettings());
+		master.add(emailSettings());
+		master.add(fileSettings());
+		master.add(importButton);
+		master.add(exportButton);
+		master.add(saveButton);
+		
+		return master;
+	}
+	
+	public JPanel nameSettings() {	
+		
+		JPanel namePanel = new JPanel();
+		JLabel nameLabel = new JLabel("Name: ");
+		JTextField nameText = new JTextField();
+		nameText.setText("DEFAULT NAME");
+		
+		namePanel.add(nameLabel);
+		namePanel.add(nameText);
+		
+		return namePanel;
+	}
+	
+	public JPanel emailSettings() {
+		JPanel emailPanel = new JPanel();
+		JLabel emailLabel = new JLabel("Email: ");
+		JTextField emailText = new JTextField();
+		emailText.setText("DEFAULT EMAIL");
+		
+		emailPanel.add(emailLabel);
+		emailPanel.add(emailText);
+
+		return emailPanel;
+	}
+	
+	public JPanel fileSettings() {
+		JPanel filePanel = new JPanel();
+		JButton fileButton = new JButton("DEFAULT FILE PATH");
+		JLabel fileLabel = new JLabel("File Path: ");
+
+		filePanel.add(fileLabel);
+		filePanel.add(fileButton);
+		
+		return filePanel;
+	}
+	
+}
