fc5d28cb21e34fa51a99bca7ab06eba59e56b57f
Anthony <nguyenanthony80@gmail.com>
2020-05-27 18:53:43 -0700

    added login button

--- HomeManual/src/components/MenuBar.java
+++ HomeManual/src/components/MenuBar.java
@@ -9,7 +9,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
 import utilities.About;
 import view.ManualGUI;
