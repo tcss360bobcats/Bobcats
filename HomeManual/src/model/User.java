b8ce7226811bb5329ac084880319a203619efd7f
Andrew Lim <2000andrewlim@gmail.com>
2020-06-01 18:52:17 -0700

    update settings code and create User model

---
+++ HomeManual/src/model/User.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * Model of a User that contains username and email.
+ * @author Andrew Lim
+ *
+ */
+public class User implements Serializable {
+	private String username;
+	private String email;
+	
+	public User(String username, String email) {
+		this.username = username;
+		this.email = email;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void updateUsername(String username) {
+		this.username = username;
+	}
+	
+	public void updateEmail(String email) {
+		this.email = email;
+	}
+}
--- HomeManual/src/utilities/Settings.java
+++ HomeManual/src/utilities/Settings.java
@@ -1,64 +1,87 @@
 package utilities;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.nio.file.FileAlreadyExistsException;
+
+import model.User;
 
 /**
- * 
+ * Settings that stores user information.
  * @author Andrew Lim
  *
  */
-public class Settings implements Serializable {
-
+public class Settings {
+	// private User user;
+	
+	public Settings() {
+		
+	}
+	
+	public void importSettings(User importUser) throws FileNotFoundException {
+		File f = new File("./users/" + importUser.getUsername() + ".ser");
+		// if user does not exist
+		if (!f.exists()) {
+			// throw exception
+			throw new FileNotFoundException();
+		} else {
+			// update files/profile.ser with the new user
+			try {
+	            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+	            User importedUser = (User) ois.readObject();
+	            FileOutputStream out = new FileOutputStream("./files/profile.ser");
+	            ObjectOutputStream oout = new ObjectOutputStream(out);
+	            oout.writeObject(importedUser);
+	            oout.close();
+	            ois.close();
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+		}	
+	}
+	
+	public void exportSettings(User exportUser) throws FileAlreadyExistsException {
+		File f = new File("./users/" + exportUser.getUsername() + ".ser");
+		// if user already exists
+		if (f.exists()) {
+			// throw exception
+			throw new FileAlreadyExistsException(f.toString());
+		} else {
+			// update files/profile.ser with the new user
+			try {
+	            FileOutputStream out = new FileOutputStream(f);
+	            ObjectOutputStream oout = new ObjectOutputStream(out);
+	            oout.writeObject(exportUser);
+	            oout.close();
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+		}
+	}
+	
 	/**
-	 * @author Andrew Lim
+	 * Main method for testing purposes only.
+	 * Will be removed in final version.
+	 * @param args
 	 */
-    private static final long serialVersionUID = 2545946456909824902L;
-    public String tagName;
-    public String email;
-    public Settings(String tagName, String email) {
-        this.tagName = tagName;
-        this.email = email;
-    }
-
-    /**
-     * @author Andrew Lim
-     */
-    public String toString() {
-        return tagName + " " + email;
-    }
-
-    /**
-     * @author Andrew Lim
-     */
-    public void exportSettings() {
-        try {
-            FileOutputStream out = new FileOutputStream("./files/" + this.tagName + ".txt");
-            ObjectOutputStream oout = new ObjectOutputStream(out);
-            oout.writeObject(this);
-            oout.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    /**
-     * @author Andrew Lim
-     */
-    public void importSettings() {
-        try {
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("./files/" + this.tagName + ".txt"));
-            Settings currentSettings = (Settings) ois.readObject();
-            FileOutputStream out = new FileOutputStream("./files/Settings.txt");
-            ObjectOutputStream oout = new ObjectOutputStream(out);
-            oout.writeObject(currentSettings);
-            oout.close();
-            ois.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	public static void main(String[] args) {
+		Settings s = new Settings();
+		User bob = new User("bob", "bob@gmail.com");
+		try {
+			s.exportSettings(bob);
+		} catch (FileAlreadyExistsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		try {
+//			s.importSettings(bob);
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
 }
