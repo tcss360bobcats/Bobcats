a18a8a2fbce52eb058687c81afd5c8c8b76b3bb3
Andrew Lim <2000andrewlim@gmail.com>
2020-06-01 21:32:44 -0700

    implement import and export button action listeners

--- HomeManual/src/components/SettingWindow.java
+++ HomeManual/src/components/SettingWindow.java
@@ -1,27 +1,41 @@
 package components;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
+import java.nio.file.FileAlreadyExistsException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import model.User;
+import utilities.Settings;
 
 public class SettingWindow extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2545946456909824902L;
+	private Settings s;
 	private User user;
 	private JLabel usernameText;
 	private JLabel emailText;
 	
+	private JTextField usernameImport;
+	private JTextField usernameExport;
+	private JTextField emailExport;
+	
+	/**
+	 * @author Anthony
+	 */
 	public SettingWindow() {
+		this.s = new Settings();
 		/**
 		 * get the user in files/profile.ser
 		 * profile.ser contains the current user of the application
@@ -34,7 +48,8 @@ public class SettingWindow extends JFrame {
 		} catch (Exception e) {
 			System.out.println(e);
 		}	
-		setSize(300, 250);
+		setSize(500, 150);
+		setResizable(false);
 		add(createWindow());
 		setVisible(true);
 	}
@@ -45,20 +60,33 @@ public class SettingWindow extends JFrame {
 	 * @return JPanel with settings options
 	 */
 	public JPanel createWindow() {
-		JPanel master = new JPanel();
+		JPanel master = new JPanel(new BorderLayout());
+		JPanel north = new JPanel();
+		north.add(usernameSettings());
+		north.add(emailSettings());
+		master.add(north, BorderLayout.NORTH);
 		JButton importButton = new JButton("Import Settings");
 		JButton exportButton = new JButton("Export Settings");
-		master.add(usernameSettings());
-		master.add(emailSettings());
-		master.add(importButton);
-		master.add(exportButton);
+		JPanel center = new JPanel();	
+		center.add(importButton);
+		center.add(exportButton);
+		master.add(center, BorderLayout.CENTER);
+		JPanel importForm = importForm();
+		JPanel exportForm = exportForm();
+		importForm.setVisible(false);
+		exportForm.setVisible(false);
+		JPanel south = new JPanel();
+		south.add(importForm);
+		south.add(exportForm);
+		master.add(south, BorderLayout.SOUTH);
 		/**
 		 * @author Andrew Lim
 		 */
 		importButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				exportForm.setVisible(false);
+				importForm.setVisible(true);
 			}
 		});
 		/**
@@ -67,10 +95,68 @@ public class SettingWindow extends JFrame {
 		exportButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				importForm.setVisible(false);
+				exportForm.setVisible(true);
 			}
 		});
 		return master;
+	}
+	
+	/**
+	 * @author Andrew Lim
+	 * @return
+	 */
+	public JPanel importForm() {
+		JPanel panel = new JPanel();
+		JLabel usernameLabel = new JLabel("Username:");
+		usernameImport = new JTextField(10);
+		JButton importButton = new JButton("Import");
+		panel.add(usernameLabel);
+		panel.add(usernameImport);
+		panel.add(importButton);
+		importButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					s.importSettings(usernameImport.getText());
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		return panel;
+	}
+	
+	/**
+	 * @author Andrew Lim
+	 * @return
+	 */
+	public JPanel exportForm() {
+		JPanel panel = new JPanel();
+		JLabel usernameLabel = new JLabel("Username:");
+		JLabel emailLabel = new JLabel("Email:");
+		usernameExport = new JTextField(10);
+		emailExport = new JTextField(10);
+		JButton exportButton = new JButton("Export");
+		panel.add(usernameLabel);
+		panel.add(usernameExport);
+		panel.add(emailLabel);
+		panel.add(emailExport);
+		panel.add(exportButton);
+		exportButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				User exportUser = new User(usernameExport.getText(), emailExport.getText());
+				try {
+					s.exportSettings(exportUser);
+				} catch (FileAlreadyExistsException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		return panel;
 	}
 	
 	/**
